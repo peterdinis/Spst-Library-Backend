aa3dd0012aa1555511beb102929a412462170725
peterdinis <petter1@pobox.sk>
2025-08-16 17:11:11 +0200

    feat: Update app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { BooksModule } from 'src/books/books.module';
 import { LoggingMiddleware } from 'src/shared/middleware/logging.middleware';
 import { CategoriesModule } from 'src/categories/categories.module';
 import { ConfigModule } from '@nestjs/config';
-import { EmailsModule } from './emails/emails.module';
+import { EmailsModule } from '../emails/emails.module';
 import { AuthorsModule } from 'src/authors/authors.module';
 import { StudentsModule } from 'src/students/students.module';
 import { TeachersModule } from 'src/teachers/teachers.module';
