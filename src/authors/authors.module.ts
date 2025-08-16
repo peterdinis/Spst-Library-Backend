41f8f2cacade6abf952144b6ca7cb07d8da82d49
peterdinis <petter1@pobox.sk>
2025-08-16 11:18:30 +0200

    feat: Start working on author module

---
+++ src/authors/authors.module.ts
@@ -0,0 +1,12 @@
+import { CacheModule } from "@nestjs/cache-manager";
+import { Module } from "@nestjs/common";
+import { PrismaModule } from "src/prisma/prisma.module";
+
+@Module({
+    imports: [PrismaModule, CacheModule.register({
+        ttl: 60, // default cache time-to-live (in seconds)
+        max: 100, // max items (if in-memory)
+    })]
+})
+
+export class AuthorsModule { }
