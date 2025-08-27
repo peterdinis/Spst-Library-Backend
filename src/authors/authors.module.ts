54cf835195b7b6ed022e8fc45cb70a029cdfda69
peterdinis <petter1@pobox.sk>
2025-08-27 12:28:54 +0200

    fix: Fixing bug in Authors.module

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -1,8 +1,12 @@
 import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthorsController } from './authors.controller';
+import { AuthorsService } from './authors.service';
 
 @Module({
   imports: [PrismaModule, CacheModule],
+  controllers: [AuthorsController],
+  providers: [AuthorsService],
 })
 export class AuthorsModule {}
