0dd331dbaab1912bd0b925ba7ece3b89e67e20cc
peterdinis <petter1@pobox.sk>
2025-09-02 20:59:53 +0200

    fix: Reinstall modules and fix swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
-  );
-  await app.listen(process.env.PORT ?? 3001);
+
+  const config = new DocumentBuilder()
+    .setTitle('SPŠT API Documentation')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
