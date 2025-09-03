58607daeba3459a76663a4f3f2e0f18a43c221d2
peterdinis <petter1@pobox.sk>
2025-09-03 17:01:58 +0200

    feat: New port

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
