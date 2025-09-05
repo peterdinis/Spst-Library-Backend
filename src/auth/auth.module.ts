dcbb1db6648cb9133871faae9837ab4d85d17533
peterdinis <petter1@pobox.sk>
2025-09-05 14:06:15 +0200

    feat: WOrking on auth logic WIP

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
-@Module({})
-export class AuthModule {}
+@Module({
+    imports: [
+        PassportModule,
+        JwtModule.register({
+            secret: process.env.JWT_SECRET || 'supersecret',
+            signOptions: { expiresIn: '1h' },
+        }),
+    ],
+    controllers: [AuthController],
+    providers: [AuthService]
+})
+
+export class AuthModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { Role } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterDto } from './dto/register-dto';
+import { LoginDto } from './dto/login-dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+
+  async register(dto: RegisterDto) {
+    const existingUser = await this.prisma.user.findUnique({ where: { email: dto.email } });
+    if (existingUser) throw new BadRequestException('Email already in use');
+
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
+
+    const user = await this.prisma.user.create({
+      data: { ...dto, password: hashedPassword, role: Role.STUDENT },
+    });
+
+    return this.generateToken(user.id, user.role);
+  }
+
+  async login(dto: LoginDto) {
+    const user = await this.prisma.user.findUnique({ where: { email: dto.email } });
+    if (!user) throw new UnauthorizedException('Invalid credentials');
+
+    const isPasswordValid = await bcrypt.compare(dto.password, user.password);
+    if (!isPasswordValid) throw new UnauthorizedException('Invalid credentials');
+
+    return this.generateToken(user.id, user.role);
+  }
+
+  async profile(userId: number) {
+    return this.prisma.user.findUnique({ where: { id: userId }, select: { id: true, email: true, name: true, role: true, createdAt: true } });
+  }
+
+  private generateToken(userId: number, role: Role) {
+    return {
+      access_token: this.jwtService.sign({ sub: userId, role }),
+    };
+  }
+}
