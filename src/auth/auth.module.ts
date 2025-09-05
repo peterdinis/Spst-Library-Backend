dbf360cd42654a838d2b86c8d8613c223b8f91e3
peterdinis <petter1@pobox.sk>
2025-09-05 21:55:42 +0200

    fix: Fixing issue in auth.module with missing import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,17 +5,20 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategy/jwt.strategy';
 import { RolesModule } from 'src/roles/roles.module';
+import { AccessControlService } from 'src/roles/access-control.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   imports: [
     PassportModule,
     RolesModule,
+    PrismaModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'supersecret',
       signOptions: { expiresIn: '1h' },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, AccessControlService],
 })
 export class AuthModule {}
