e448bdc810bdb5b9cd360693c5ed7aee22b6627b
peterdinis <petter1@pobox.sk>
2025-09-15 17:01:46 +0200

    feat: Basic authors-suggestion module is DONE

--- src/authors-suggestion/authors-suggestion.module.ts
+++ src/authors-suggestion/authors-suggestion.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "src/prisma/prisma.module";
+import { AuthorSuggestionService } from "./authors-suggestion.service";
+import { AuthorSuggestionController } from "./authors-suggestion.controller";
 
 @Module({
     imports: [PrismaModule],
-    providers: [],
-    controllers: []
+    providers: [AuthorSuggestionService],
+    controllers: [AuthorSuggestionController]
 })
 
 export class AuthorsSuggestion {}
