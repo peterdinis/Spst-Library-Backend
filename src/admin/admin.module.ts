59123d2b3f4e37f3928510021769a5b581e5cfd0
peterdinis <petter1@pobox.sk>
2025-09-24 21:42:31 +0200

    feat: Start working on admin module WIP

---
+++ src/admin/admin.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { PrismaModule } from "src/prisma/prisma.module";
+import { AdminService } from "./admin.service";
+import { AdminController } from "./admin.controller";
+
+@Module({
+    imports: [PrismaModule],
+    providers: [AdminService],
+    controllers: [AdminController]
+})
+
+export class AdminModule {}
