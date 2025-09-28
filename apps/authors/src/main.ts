b7d3253204872dcff185586173a602a32c4f1144
peterdinis <petter1@pobox.sk>
2025-09-28 17:06:00 +0200

    infra: Updating swagger in backend WIP

--- apps/authors/src/main.ts
+++ apps/authors/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AuthorsModule } from './authors.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthorsModule);
-  await app.listen(process.env.port ?? 3000);
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('SPŠT API Documentation Authors')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 5002);
 }
 bootstrap();
