7b142c29312da537cb198b67f81df074e741c688
peterdinis <petter1@pobox.sk>
2025-09-28 17:10:22 +0200

    feat: Updating swagger part in apps

--- apps/pdf/src/main.ts
+++ apps/pdf/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { PdfModule } from './pdf.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(PdfModule);
-  await app.listen(process.env.port ?? 3000);
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('SPŠT API Documentation Pdfs')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 5008);
 }
 bootstrap();
