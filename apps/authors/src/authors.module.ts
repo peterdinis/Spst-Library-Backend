2949dd84204abf6010dbaa97d8e28e30ec149470
peterdinis <petter1@pobox.sk>
2025-09-29 12:42:32 +0200

    fix: Fixing issues in modules

--- apps/authors/src/authors.module.ts
+++ apps/authors/src/authors.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthorsController } from './authors.controller';
 import { AuthorsService } from './authors.service';
 import { DatabaseModule } from '@app/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Author, AuthorSchema } from './models/author.model';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule,
+    MongooseModule.forFeature([
+      {
+        name: Author.name, schema: AuthorSchema
+      }
+    ])
+  ],
   controllers: [AuthorsController],
   providers: [AuthorsService],
 })
